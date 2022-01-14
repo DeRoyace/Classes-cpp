3fb90566b7cbf6971fb92a635adb37bb30d0c2e2
Rohit <creative.roy2003@gmail.com>
2022-01-14 13:32:27 +0530

    functions in C++

---
+++ friendfunc.cpp
@@ -0,0 +1,64 @@
+/**
+ * @file friendfunc.cpp
+ * @author Rohit,Rohit (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-01-14
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+Friend Class A friend class can access private and protected members of other class in which it is declared as friend. It is sometimes useful to allow a particular class to access private members of other class.
+
+Friend Function Like friend class, a friend function can be given a special grant to access private and protected members. A friend function can be: 
+a) A member of another class 
+b) A global function 
+
+Following are some important points about friend functions and classes: 
+1) Friends should be used only for limited purpose. too many functions or external classes are declared as friends of a class with protected or private data, it lessens the value of encapsulation of separate classes in object-oriented programming.
+2) Friendship is not mutual. If class A is a friend of B, then B doesn't become a friend of A automatically.
+3) Friendship is not inherited (See this for more details)
+4) The concept of friends is not there in Java.
+*/
+
+#include<iostream>
+using namespace std;
+
+class ClassB;
+
+class ClassA {
+    
+    public:
+        // constructor to initialize numA to 12
+        ClassA() : numA(12) {}
+        
+    private:
+        int numA;
+        
+         // friend function declaration
+         friend int add(ClassA, ClassB);
+};
+
+class ClassB {
+
+    public:
+        // constructor to initialize numB to 1
+        ClassB() : numB(1) {}
+    
+    private:
+        int numB;
+ 
+        // friend function declaration
+        friend int add(ClassA, ClassB);
+};
+
+// access members of both classes
+int add(ClassA objectA, ClassB objectB) {
+    return (objectA.numA + objectB.numB);
+}
+
+int main() {
+    ClassA objectA;
+    ClassB objectB;
+    cout << "Sum: " << add(objectA, objectB);
+    return 0;
+}
---
+++ virtualFunc.cpp
@@ -0,0 +1,50 @@
+/*
+A virtual function is a member function which is declared within a base class and is re-defined (overridden) by a derived class. When you refer to a derived class object using a pointer or a reference to the base class, you can call a virtual function for that object and execute the derived class's version of the function. 
+
+Virtual functions ensure that the correct function is called for an object, regardless of the type of reference (or pointer) used for function call.
+
+They are mainly used to achieve Runtime polymorphism
+Functions are declared with a virtual keyword in base class.
+The resolving of function call is done at runtime.
+
+Rules for Virtual Functions are:
+
+1. Virtual functions cannot be static.
+2. A virtual function can be a friend function of another class.
+3. Virtual functions should be accessed using pointer or reference of base class type to achieve runtime polymorphism.
+4. The prototype of virtual functions should be the same in the base as well as derived class.
+5. They are always defined in the base class and overridden in a derived class. It is not mandatory for the derived   class to override (or re-define the virtual function), in that case, the base class version of the function is used.
+6. A class may have virtual destructor but it cannot have a virtual constructor.
+
+Source Notes: https://www.geeksforgeeks.org/virtual-function-cpp/
+*/
+
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+    virtual void display()
+    {
+        cout << "Base class is invoked" << endl;
+    }
+};
+
+class B : public A
+{
+public:
+    void display()
+    {
+        cout << "Derived Class is invoked" << endl;
+    }
+};
+
+int main()
+{
+    A *a; //pointer of base class
+    B b;  //object of derived class
+    a = &b;
+    a->display(); //Late Binding occurs
+    return 0;
+}
